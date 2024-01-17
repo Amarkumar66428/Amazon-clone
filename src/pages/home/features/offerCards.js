82213fc98114ba040dedb7f4a542a4d89633fb34
amar kumar <amar@apptunix.com>
2024-01-18 01:18:55 +0530

    card mofification

--- src/pages/home/features/offerCards.js
+++ src/pages/home/features/offerCards.js
@@ -9,10 +9,10 @@ const OfferCards = () => {
                     <div className='top_offers'>
                         <HomeCards
                             title='Starting ₹199 | Amazon brands & more'
-                            offerOne='https://m.media-amazon.com/images/I/51cQVMHW-SL._AC_UL320_.jpg'
-                            offerTwo='https://m.media-amazon.com/images/I/41hyw+KaYEL._AC_UL320_.jpg'
-                            offerThree='https://m.media-amazon.com/images/I/516lsFISODL._AC_UL320_.jpg'
-                            offerFour='https://m.media-amazon.com/images/I/812bkK1hGKL._AC_UL320_.jpg' />
+                            offerOne='../images/mob2.jpg'
+                            offerTwo='../images/mob3.jpg'
+                            offerThree='../images/mob3.jpg'
+                            offerFour='../images/mob2.jpg' />
                         <HomeCards
                             title='Up to 75% off | Headphones'
                             offerOne='https://m.media-amazon.com/images/I/51cQVMHW-SL._AC_UL320_.jpg'
